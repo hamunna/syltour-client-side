4691b064eaa5ab58ed1830a22ce4a918162ef0c5
hamunna <hamunna@gmail.com>
2021-11-04 12:59:44 +0600

    Add New Service Input Reference and Form Hnadler Added

--- src/pages/AddNewService/AddNewService.js
+++ src/pages/AddNewService/AddNewService.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Container } from 'react-bootstrap';
 
 const AddNewService = () => {
+
+	const [tours, setTours] = useState([]);
+
+	const nameRef = useRef();
+	const descriptionRef = useRef();
+	const basicPriceRef = useRef();
+	const discountPriceRef = useRef();
+	const imageRef = useRef();
+
+	const handleAddService = e => {
+		e.preventDefault();
+
+		const name = nameRef.current.value;
+		const description = descriptionRef.current.value;
+		const basicPrice = basicPriceRef.current.value;
+		const discountPrice = discountPriceRef.current.value;
+		const image = imageRef.current.value;
+
+		const newService = { name, description, basicPrice, discountPrice, image };
+
+		fetch('')
+	}
+
 	return (
 		<div className="my-5 py-2">
 
@@ -9,27 +32,33 @@ const AddNewService = () => {
 
 				<h1 className="my-5 text-center fw-bold theme-secondary-text border-bottom pb-4">Add New Service</h1>
 
-				<form className="w-50 mx-auto">
+				<form onSubmit={handleAddService} className="w-50 mx-auto">
 
 					<div class="row mb-3">
 						<div class="col-sm-10 col-md-12">
-							<input type="text" class="form-control" placeholder="Place Name" />
+							<input ref={nameRef} type="text" class="form-control" placeholder="Place Name" />
 						</div>
 					</div>
 
 					<div class="row mb-3">
 						<div class="col-sm-10 col-md-12">
-							<textarea rows="5" className="w-100 form-control" placeholder="Description"></textarea>
+							<textarea ref={descriptionRef} rows="5" className="w-100 form-control" placeholder="Description"></textarea>
 						</div>
 					</div>
 
 					<div class="row">
 						<div class="col">
-							<input type="text" class="form-control" placeholder="Basic Price" />
+							<input ref={basicPriceRef} type="text" class="form-control" placeholder="Basic Price" />
 						</div>
 
 						<div class="col">
-						<input type="text" class="form-control" placeholder="Discount Price" />
+							<input ref={discountPriceRef} type="text" class="form-control" placeholder="Discount Price" />
+						</div>
+					</div>
+
+					<div class="row mb-3">
+						<div class="col-sm-10 col-md-12">
+							<input ref={imageRef} type="url" class="form-control" placeholder="Image URL" />
 						</div>
 					</div>
 
