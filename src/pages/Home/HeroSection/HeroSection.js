1bc5ca44567a08f40ed60ffcb32e952161ad5ccd
hamunna <hamunna@gmail.com>
2021-11-06 17:04:56 +0600

    Hero Section Fixed

--- src/pages/Home/HeroSection/HeroSection.js
+++ src/pages/Home/HeroSection/HeroSection.js
@@ -8,8 +8,12 @@ const HeroSection = () => {
 				<Container>
 					<div className="hero-content text-uppercase text-white">
 						<h1>Welcome!</h1>
-						<h3>to the land of</h3>
-						<h1>360 Awlia</h1>
+						<span>to the land of <em>360</em> Awlia</span>
+
+						<div id="hero-email-filed">
+							<input type="email" name="get-offers" id="get-offers" placeholder="Your Email" />
+							<button className="btn-pill">Get Travel Tips</button>
+						</div>
 					</div>
 				</Container>
 			</div>
